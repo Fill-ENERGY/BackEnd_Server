fafd14581487ce131e5997074e94ecf23109d1da
mashin2002 <mashin0128@gmail.com>
2024-08-06 18:27:06 +0900

    ♻️ refactor : Report 엔티티 구조 변경

---
+++ src/main/java/com/example/template/domain/report/entity/ReportCategory.java
@@ -0,0 +1,12 @@
+package com.example.template.domain.report.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ReportCategory {
+    MEMBER,
+    BOARD,
+    COMMENT
+}
