89b563790fa6d92576e82c3619fc8ae78261d0dd
Jeongmo Seo <wjdah1402@gmail.com>
2024-08-07 21:05:56 +0900

    :bug: fix: 충전소 쿼리의 table 이름 수정

--- src/main/java/com/example/template/domain/station/repository/StationRepository.java
+++ src/main/java/com/example/template/domain/station/repository/StationRepository.java
@@ -1,8 +1,6 @@
 package com.example.template.domain.station.repository;
 
 import com.example.template.domain.station.entity.Station;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,11 +14,11 @@ public interface StationRepository extends JpaRepository<Station, Long> {
 
     List<Station> findAllByOrderByScoreDesc();
 
-    @Query(value = "SELECT s1.* FROM Station s1 JOIN (SELECT s2.station_id, (6371 * acos(cos(radians(:latitude)) * cos(radians(s2.latitude)) * cos(radians(s2.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(s2.latitude)))) AS distance FROM Station s2) AS stations ON s1.station_id = stations.station_id WHERE stations.distance > (SELECT (6371 * acos(cos(radians(:latitude)) * cos(radians(s2.latitude)) * cos(radians(s2.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(s2.latitude)))) AS distance FROM Station s2 WHERE s2.station_id = :lastId) ORDER BY stations.distance ASC LIMIT :offset",
+    @Query(value = "SELECT s1.* FROM station s1 JOIN (SELECT s2.station_id, (6371 * acos(cos(radians(:latitude)) * cos(radians(s2.latitude)) * cos(radians(s2.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(s2.latitude)))) AS distance FROM station s2) AS stations ON s1.station_id = stations.station_id WHERE stations.distance > (SELECT (6371 * acos(cos(radians(:latitude)) * cos(radians(s2.latitude)) * cos(radians(s2.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(s2.latitude)))) AS distance FROM station s2 WHERE s2.station_id = :lastId) ORDER BY stations.distance ASC LIMIT :offset",
             nativeQuery = true)
     Slice<Station> findAllOrderByDistanceFromId(@Param("latitude") double latitude, @Param("longitude") double longitude, Long lastId, int offset);
 
-    @Query(value = "SELECT s1.* FROM Station s1 JOIN (SELECT s2.station_id, (6371 * acos(cos(radians(:latitude)) * cos(radians(s2.latitude)) * cos(radians(s2.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(s2.latitude)))) AS distance FROM Station s2) AS stations ON s1.station_id = stations.station_id ORDER BY stations.distance ASC LIMIT :offset",
+    @Query(value = "SELECT s1.* FROM station s1 JOIN (SELECT s2.station_id, (6371 * acos(cos(radians(:latitude)) * cos(radians(s2.latitude)) * cos(radians(s2.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(s2.latitude)))) AS distance FROM station s2) AS stations ON s1.station_id = stations.station_id ORDER BY stations.distance ASC LIMIT :offset",
             nativeQuery = true)
     Slice<Station> findAllByOrderByDistance(@Param("latitude") double latitude, @Param("longitude") double longitude, int offset);
 }
