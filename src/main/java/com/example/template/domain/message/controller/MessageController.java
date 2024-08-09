752de719575bd2a523d0f4721da5e333b5ea9b7f
이승연 <dltmddus9082@naver.com>
2024-08-10 03:19:46 +0900

    ♻️️ refactor: swagger summary 변경

--- src/main/java/com/example/template/domain/message/controller/MessageController.java
+++ src/main/java/com/example/template/domain/message/controller/MessageController.java
@@ -33,7 +33,7 @@ public class MessageController {
     private final UnmappedImageCleanupScheduler unmappedImageCleanupScheduler;
 
 
-    @Operation(summary = "쪽지 이미지 업로드 API", description = "쪽지로 전송할 이미지를 저장합니다.")
+    @Operation(summary = "쪽지 이미지 업로드", description = "쪽지로 전송할 이미지를 저장합니다.")
     @PostMapping(value = "messages/images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ApiResponse<MessageResponseDTO.MessageImgDTO> uploadImages(@RequestPart("images") List<MultipartFile> images,
                                                                       @AuthenticatedMember Member member) {
@@ -41,7 +41,7 @@ public class MessageController {
     }
 
     @PostMapping(value = "/messages")
-    @Operation(summary = "쪽지 전송 API", description = "쪽지를 전송합니다. ")
+    @Operation(summary = "쪽지 전송", description = "쪽지를 전송합니다. ")
     public ApiResponse<MessageResponseDTO.MessageDTO> createMessage(@Valid @RequestBody MessageRequestDTO.CreateMessageDTO requestDTO,
                                                                     @AuthenticatedMember Member member) {
         MessageResponseDTO.MessageDTO messageDTO = messageCommandService.createMessage(requestDTO, member);
@@ -49,14 +49,14 @@ public class MessageController {
     }
 
     @GetMapping("/messages/{messageId}")
-    @Operation(summary = "쪽지 단일 조회 API")
+    @Operation(summary = "쪽지 단일 조회")
     public ApiResponse<MessageResponseDTO.MessageDTO> getMessage(@PathVariable("messageId") Long messageId, @AuthenticatedMember Member member) {
         MessageResponseDTO.MessageDTO messageDTO= messageQueryService.getMessage(messageId, member);
         return ApiResponse.onSuccess(messageDTO);
     }
 
     @GetMapping("/threads/{threadId}/messages")
-    @Operation(summary = "쪽지 목록 조회 API", description = "채팅방의 쪽지 목록을 조회합니다.")
+    @Operation(summary = "쪽지 목록 조회", description = "채팅방의 쪽지 목록을 조회합니다.")
     @Parameters({
             @Parameter(name = "cursor", description = "마지막 쪽지 id(messageId)"),
             @Parameter(name = "limit", description = "가져올 채팅방 개수, default = 10"),
@@ -70,7 +70,7 @@ public class MessageController {
     }
 
     @PatchMapping("/threads/{threadId}/messages")
-    @Operation(summary = "쪽지 목록 조회 및 읽음 상태 업데이트 API", description = "채팅방의 쪽지 목록을 조회하고 읽지 않은 쪽지를 읽음으로 업데이트합니다.")
+    @Operation(summary = "쪽지 목록 조회 및 읽음 상태 업데이트", description = "채팅방의 쪽지 목록을 조회하고 읽지 않은 쪽지를 읽음으로 업데이트합니다.")
     @Parameters({
             @Parameter(name = "cursor", description = "마지막 쪽지 id(messageId)"),
             @Parameter(name = "limit", description = "가져올 채팅방 개수, default = 10"),
@@ -84,7 +84,7 @@ public class MessageController {
     }
 
     @GetMapping("/threads/members/{memberId}")
-    @Operation(summary = "채팅방 조회 API (커뮤니티 > 채팅하기)", description = "게시글 작성자의 id를 전달해주세요.\n\n" +
+    @Operation(summary = "채팅방 조회 (커뮤니티 > 채팅하기)", description = "게시글 작성자의 id를 전달해주세요.\n\n" +
             "커뮤니티에서 채팅하기를 클릭하는 경우, 게시글 작성자와의 채팅방이 존재하는지 조회합니다.\n\n" + "존재하면 채팅방 id를, 존재하지 않으면 null을 반환합니다.")
     public ApiResponse<MessageResponseDTO.ThreadDTO> getThread(@PathVariable("memberId") Long writerId, @AuthenticatedMember Member member) {
         MessageResponseDTO.ThreadDTO threadDTO = messageQueryService.getThread(writerId, member);
@@ -92,7 +92,7 @@ public class MessageController {
     }
 
     @GetMapping("/threads")
-    @Operation(summary = "채팅방 목록 조회 API")
+    @Operation(summary = "채팅방 목록 조회")
     @Parameters({
             @Parameter(name = "cursor", description = "마지막 채팅방의 updatedAt 값. 첫 페이지 조회 시에는 값을 비워서 전달해주세요."),
             @Parameter(name = "lastId", description = "마지막 채팅방의 id(threadId), default = 0"),
@@ -113,14 +113,14 @@ public class MessageController {
     }
 
     @PatchMapping("/messages/{messageId}")
-    @Operation(summary = "쪽지 삭제(soft delete) API", description = "쪽지를 논리적으로 삭제합니다. 쪽지를 삭제된 것으로 표시합니다.")
+    @Operation(summary = "쪽지 삭제(soft delete)", description = "쪽지를 논리적으로 삭제합니다. 쪽지를 삭제된 것으로 표시합니다.")
     public ApiResponse<MessageResponseDTO.MessageDeleteDTO> softDeleteMessage(@PathVariable("messageId") Long messageId, @AuthenticatedMember Member member) {
         MessageResponseDTO.MessageDeleteDTO messageDeleteDTO= messageCommandService.softDeleteMessage(messageId, member);
         return ApiResponse.onSuccess(messageDeleteDTO);
     }
 
     @PatchMapping("/threads/{threadId}")
-    @Operation(summary = "채팅방 나가기(soft delete) API", description = "채팅방을 논리적으로 삭제합니다. 채팅방을 나간 것으로 표시합니다.")
+    @Operation(summary = "채팅방 나가기(soft delete)", description = "채팅방을 논리적으로 삭제합니다. 채팅방을 나간 것으로 표시합니다.")
     public ApiResponse<MessageResponseDTO.ThreadDeleteDTO> softDeleteThread(@PathVariable("threadId") Long threadId, @AuthenticatedMember Member member) {
         MessageResponseDTO.ThreadDeleteDTO threadDeleteDTO= messageCommandService.softDeleteThread(threadId, member);
         return ApiResponse.onSuccess(threadDeleteDTO);
@@ -128,21 +128,21 @@ public class MessageController {
 
     // TODO: 스케줄러 테스트용 api
     @PostMapping("/messages/hard-delete-thread")
-    @Operation(summary = "채팅방 삭제(hard delete) 스케쥴러 테스트용 API", description = "(연동x) 채팅방을 물리적으로 삭제합니다. 참여자가 없는 채팅방을 삭제합니다.")
+    @Operation(summary = "채팅방 삭제(hard delete) 스케쥴러 테스트용", description = "(연동x) 채팅방을 물리적으로 삭제합니다. 참여자가 없는 채팅방을 삭제합니다.")
     public ApiResponse<String> triggerHardDeleteThread() {
         messageHardDeleteScheduler.hardDeleteThread();
         return ApiResponse.onSuccess("채팅방 삭제 스케쥴러 호출");
     }
 
     @PostMapping("/messages/hard-delete-message")
-    @Operation(summary = "쪽지 삭제(hard delete) 스케쥴러 테스트용 API", description = "(연동x) 쪽지를 물리적으로 삭제합니다. 보낸 사람과 받는 사람이 모두 삭제한 쪽지를 삭제합니다.")
+    @Operation(summary = "쪽지 삭제(hard delete) 스케쥴러 테스트용", description = "(연동x) 쪽지를 물리적으로 삭제합니다. 보낸 사람과 받는 사람이 모두 삭제한 쪽지를 삭제합니다.")
     public ApiResponse<String> triggerHardDeleteMessage() {
         messageHardDeleteScheduler.hardDeleteMessage();
         return ApiResponse.onSuccess("쪽지 삭제 스케쥴러 호출");
     }
 
     // TODO :쪽지 이미지 삭제 스케줄러 테스트용 api
-    @Operation(summary = "쪽지 이미지 삭제 스케쥴러 테스트용 API", description = "(연동x) message와 매핑이 안된 messageimg를 삭제합니다.")
+    @Operation(summary = "쪽지 이미지 삭제 스케쥴러 테스트용", description = "(연동x) message와 매핑이 안된 messageimg를 삭제합니다.")
     @PostMapping("/messages/cleanup-unmapped-images")
     public ApiResponse<String> cleanupUnmappedImages() {
         unmappedImageCleanupScheduler.cleanupUnmappedMessageImages();
