ad62ab9ffa81f77e41d540fd84a2bc16297e0539
jinho7 <69239293+jinho7@users.noreply.github.com>
2024-08-13 01:07:03 +0900

    ✨ feat: unmappedCommentImages Scheduler 구현

--- src/main/java/com/example/template/domain/board/repository/CommentImgRepository.java
+++ src/main/java/com/example/template/domain/board/repository/CommentImgRepository.java
@@ -1,6 +1,5 @@
 package com.example.template.domain.board.repository;
 
-import com.example.template.domain.board.entity.BoardImg;
 import com.example.template.domain.board.entity.CommentImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +8,6 @@ import java.util.List;
 
 public interface CommentImgRepository extends JpaRepository<CommentImg, Long> {
     List<CommentImg> findAllByCommentImgUrlIn(List<String> imageUrls);
-    @Query("SELECT b FROM BoardImg b WHERE b.board IS NULL")
+    @Query("SELECT b FROM CommentImg b WHERE b.comment IS NULL")
     List<CommentImg> findUnmappedImages();
 }
--- src/main/java/com/example/template/domain/board/scheduler/UnmappedImageCleanupScheduler.java
+++ src/main/java/com/example/template/domain/board/scheduler/UnmappedImageCleanupScheduler.java
@@ -1,7 +1,9 @@
 package com.example.template.domain.board.scheduler;
 
 import com.example.template.domain.board.entity.BoardImg;
+import com.example.template.domain.board.entity.CommentImg;
 import com.example.template.domain.board.repository.BoardImgRepository;
+import com.example.template.domain.board.repository.CommentImgRepository;
 import com.example.template.global.config.aws.S3Manager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,18 +19,27 @@ import java.util.List;
 public class UnmappedImageCleanupScheduler {
 
     private final BoardImgRepository boardImgRepository;
+    private final CommentImgRepository commentImgRepository;
     private final S3Manager s3Manager;
 
     @Scheduled(cron = "0 0 3 * * ?") // 매일 새벽 3시에 실행
     @Transactional
     public void cleanupUnmappedImages() {
-        List<BoardImg> unmappedImages = boardImgRepository.findUnmappedImages();
-
-        for (BoardImg image : unmappedImages) {
+        // 게시글 이미지 정리
+        List<BoardImg> unmappedBoardImages = boardImgRepository.findUnmappedImages();
+        for (BoardImg image : unmappedBoardImages) {
             s3Manager.deleteFile(image.getBoardImgUrl());
             boardImgRepository.delete(image);
         }
 
-        log.info("[cleanupUnmappedImages 실행] 삭제완료");
+        // 댓글 이미지 정리
+        List<CommentImg> unmappedCommentImages = commentImgRepository.findUnmappedImages();
+        for (CommentImg image : unmappedCommentImages) {
+            s3Manager.deleteFile(image.getCommentImgUrl());
+            commentImgRepository.delete(image);
+        }
+
+        log.info("[cleanupUnmappedImages 실행] 삭제된 게시글 이미지 수: {}, 댓글 이미지 수: {}",
+                unmappedBoardImages.size(), unmappedCommentImages.size());
     }
 }
