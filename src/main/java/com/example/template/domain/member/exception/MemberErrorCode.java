ff3f4a9e68d800b09b3f87e5637a018c4bf222ba
jiiiiiw <kijiwi1@naver.com>
2024-08-19 18:04:13 +0900

    ♻️️ refactor: MemberErrorCode 오류 value 수정 (#80)

--- src/main/java/com/example/template/domain/member/exception/MemberErrorCode.java
+++ src/main/java/com/example/template/domain/member/exception/MemberErrorCode.java
@@ -14,13 +14,13 @@ public enum MemberErrorCode implements BaseErrorCode {
     _INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
 
     //해당 멤버가 없을 때
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER401", "해당 멤버를 찾을 수 없습니다"),
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER404", "해당 멤버를 찾을 수 없습니다"),
     //비밀번호 재확인이 올바르지 않을 때
-    PASSWORD_NOT_EQUAL(HttpStatus.NOT_FOUND, "MEMBER402", "비밀번호가 일치하지 않습니다."),
+    PASSWORD_NOT_EQUAL(HttpStatus.NOT_FOUND, "MEMBER404", "비밀번호가 일치하지 않습니다."),
     // 이메일 없을 때
-    EMAIL_NOT_EXIST(HttpStatus.BAD_REQUEST, "USER405", "사용자 이메일이 존재하지 않습니다."),
+    EMAIL_NOT_EXIST(HttpStatus.BAD_REQUEST, "USER400", "사용자 이메일이 존재하지 않습니다."),
     //이메일이 중복될 때
-    USER_ALREADY_EXIST(HttpStatus.NOT_FOUND, "MEMBER403", "사용자가 이미 존재합니다."),
+    USER_ALREADY_EXIST(HttpStatus.NOT_FOUND, "MEMBER404", "사용자가 이미 존재합니다."),
     //비밀번호를 잘 못 입력했을 때
     PASSWORD_NOT_MATCH(HttpStatus.NOT_FOUND, "MEMBER404", "비밀번호가 일치하지 않습니다.")
     ;
